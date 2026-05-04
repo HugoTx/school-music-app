cfeb2709c634e1ce50f03788fc2d87a9fd04ecd3
HugoTx <hugotx87@gmail.com>
2026-05-04 15:48:50 +0100

    feat: add finance payments export endpoint

--- app/Http/Controllers/FinanceReportController.php
+++ app/Http/Controllers/FinanceReportController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Services\FinanceMetricsService;
 use Illuminate\Http\Request;
+use App\Exports\FinancePaymentsExport;
+use Maatwebsite\Excel\Facades\Excel;
 
 class FinanceReportController extends Controller
 {
@@ -18,5 +20,18 @@ class FinanceReportController extends Controller
         $data = $this->financeMetricsService->getDashboardData($selectedYear);
 
         return view('reports.finance', $data);
+    }
+    public function export(Request $request)
+    {
+        $selectedYear = $request->integer('year');
+
+        $filename = $selectedYear
+            ? "pagamentos-financeiro-{$selectedYear}.xlsx"
+            : 'pagamentos-financeiro.xlsx';
+
+        return Excel::download(
+            new FinancePaymentsExport($selectedYear),
+            $filename
+        );
     }
 }
